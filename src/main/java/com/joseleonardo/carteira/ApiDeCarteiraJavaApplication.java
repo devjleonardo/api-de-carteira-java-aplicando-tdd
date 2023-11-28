66dde9f862d58660652f9d1fff9fb47880d64368
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2023-11-28 02:47:53 -0300

    Criação do projeto

---
+++ src/main/java/com/joseleonardo/carteira/ApiDeCarteiraJavaApplication.java
@@ -0,0 +1,13 @@
+package com.joseleonardo.carteira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDeCarteiraJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDeCarteiraJavaApplication.class, args);
+	}
+
+}
