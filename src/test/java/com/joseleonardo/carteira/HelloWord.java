3b7d5d2de21179bc7d599e6476aa77ab710d5a69
José Leonardo <leonardo_cordeirobahia@hotmail.com>
2023-11-28 03:18:53 -0300

    Configurando o projeto para rodar os testes

--- src/test/java/com/joseleonardo/carteira/HelloWord.java
+++ src/test/java/com/joseleonardo/carteira/HelloWord.java
@@ -1,18 +1,19 @@
 package com.joseleonardo.carteira;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ActiveProfiles("test")
-class ApiDeCarteiraJavaApplicationTests {
+public class HelloWord {
 
 	@Test
-	public void contextLoads() {
+	public void testHelloWorld() {
+		assertEquals(1, 1);
 	}
-
+	
 }
